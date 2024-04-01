7a3e56e8545ba05c70334ba17b7778a1a18f696f
brian nelson <brian2656@hotmail.com>
2024-04-01 17:59:01 -0600

    return the proper updated balance

--- cd_account.py
+++ cd_account.py
@@ -30,4 +30,5 @@ def create_cd_account(balance, interest_rate, months):
     # ADD YOUR CODE HERE
     cd_account.set_interest(interest_earned)
     # Return the updated balance and interest earned.
-    return  cd_account
+    return updated_balance, interest_earned
+
