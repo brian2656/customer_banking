1584aff9cd57b828c9c447e240894e20690deed2
brian nelson <brian2656@hotmail.com>
2024-04-03 14:55:40 -0600

    adding code to the customer banking file

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from cd_account import create_cd_account 
+from savings_account import create_savings_account
 
 # Define the main function
 def main():
@@ -7,23 +9,32 @@ def main():
     and the length of months to determine the interest gained.
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
+
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
-    # Call the create_savings_account function and pass the variables from the user.
+    savings_balance = float(input ("initial savings balance ")) 
+    savings_interest = float(input ("interest rate "))
+    savings_maturity = int(input ("number of months "))
+        # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print (interest_earned)
+    print (updated_savings_balance)
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance = float(input ("input cd balance "))
+    cd_interest = float(input ("cd interest "))
+    cd_maturity = int(input ("cd maturity "))
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print (interest_earned)
+    print (updated_cd_balance)
 if __name__ == "__main__":
     # Call the main function.
+    main()
